e822ea9fe08be64209f4e4f4255292b0bec6c779
Piyush Deshmukh <99667276+Piyush-Deshmukh@users.noreply.github.com>
2023-12-24 18:21:47 +0530

    Delete client directory

--- client/src/utils/links.jsx
+++
@@ -1,8 +0,0 @@
-const links = [
-  { text: 'add note.', path: '.' },
-  { text: 'notes.', path: 'all-notes' },
-  { text: 'admin.', path: 'admin' },
-  { text: 'profile.', path: 'profile' },
-];
-
-export default links;
